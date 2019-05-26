097f4eba4af7255a04c4689886580eee454ecec0
raerae1205 <crashdean@hotmail.com>
2019-05-25 22:13:29 -0700

    Updated apiRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+//  This file holds the data from the survey.html file
+var friends = [];
+
+// The module.exports makes the friends.js file accessible to all the files.
+module.exports = friends;
+
+
+
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,59 @@
-var friends = require("../data/tableData");
-
-
+var friends = require("../data/friends");
 
 
 module.exports = function(app) {
   
 
+
   app.get("/api/friends", function(req, res) {
-    res.json(tableData);
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
     
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
+  
+  )};
+
+
+// Here we create a var for setting the friendMatch
+  var friendMatch = {
+    name: "",
+    photo: "",
+    friendDifference: Infinity
+  };
+// We did through the requset data to find survey data
+  var newData = req.body;
+  var newScores = newData.scores;
+// We create a varible to find the diffence between the new friend and the friends in the array
+  var totalDifference;
+// We run through the array and check for current friend score
+  for (var i = 0; i < friends.length; i++) {
+    var currentFriend = friends[i];
+    totalDifference = 0;
+// We console.log the currentFriend name
+    console.log(currentFriend.name);
+// We create a varible to store the currentFriend score in
+// We also create a varible and set it to the current user score
+    for (var j = 0; j < currentFriend.scores.length; j++) {
+      var currentFriendScore = currentFriend.scores[j];
+      var currentUserScore = newScores[j];
+// We run some math to find the diffenence between current user and current friend.
+      totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
     }
-  });
+// If they are the least amount of diffenence, then the match is made and the data is pushed to the friendsMatch JSON
+    if (totalDifference <= friendMatch.friendDifference) {
+      
+      friendMatch.name = currentFriend.name;
+      friendMatch.photo = currentFriend.photo;
+      friendMatch.friendDifference = totalDifference;
+    }
+  }
+
+  friends.push(userData);
+
+  res.json(friendMatch);
+
 
   
 
-  app.post("/api/clear", function(req, res) {
-    
-    tableData.length = [];
-    waitListData.length = [];
-
-    res.json({ ok: true });
-  });
-};
